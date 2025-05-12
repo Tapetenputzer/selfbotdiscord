a6a5992cce5dbf67688d25346f280d009cfe2c46
Tapetenputzer <100005334+Tapetenputzer@users.noreply.github.com>
2025-05-12 07:44:01 +0200

    Update selfbotdiscord.cpp
    added custom delete time

--- selfbotdiscord.cpp
+++ selfbotdiscord.cpp
@@ -30,28 +30,17 @@ DWORD HttpRequest(
     const std::wstring& headers,
     const std::string& body
 ) {
-    HINTERNET connect = WinHttpConnect(session, host.c_str(),
-        INTERNET_DEFAULT_HTTPS_PORT, 0);
+    HINTERNET connect = WinHttpConnect(session, host.c_str(), INTERNET_DEFAULT_HTTPS_PORT, 0);
     if (!connect) return 0;
-    HINTERNET request = WinHttpOpenRequest(
-        connect, verb.c_str(), path.c_str(),
-        nullptr, WINHTTP_NO_REFERER,
-        WINHTTP_DEFAULT_ACCEPT_TYPES,
-        WINHTTP_FLAG_SECURE
-    );
+    HINTERNET request = WinHttpOpenRequest(connect, verb.c_str(), path.c_str(), nullptr, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE);
     if (!request) {
         WinHttpCloseHandle(connect);
         return 0;
     }
-    WinHttpSendRequest(request,
-        headers.c_str(), (ULONG)-1,
-        (LPVOID)body.data(), (ULONG)body.size(),
-        (ULONG)body.size(), 0);
+    WinHttpSendRequest(request, headers.c_str(), (ULONG)-1, (LPVOID)body.data(), (ULONG)body.size(), (ULONG)body.size(), 0);
     WinHttpReceiveResponse(request, nullptr);
     DWORD status = 0, len = sizeof(status);
-    WinHttpQueryHeaders(request,
-        WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
-        nullptr, &status, &len, nullptr);
+    WinHttpQueryHeaders(request, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER, nullptr, &status, &len, nullptr);
     WinHttpCloseHandle(request);
     WinHttpCloseHandle(connect);
     return status;
@@ -72,9 +61,9 @@ int wmain() {
         std::ofstream out(TEXT_PATH);
         out << "Type your messages here.\n";
         std::cout << "No Text.txt found – created at:\n"
-                  << "  " << TEXT_PATH.string() << "\n\n"
-                  << "Please fill it with your messages and press ENTER to exit."
-                  << std::endl;
+            << "  " << TEXT_PATH.string() << "\n\n"
+            << "Please fill it with your messages and press ENTER to exit."
+            << std::endl;
         std::cin.get();
         return 1;
     }
@@ -88,7 +77,7 @@ int wmain() {
     fin.close();
     if (lines.empty()) {
         std::cerr << TEXT_PATH.filename().string()
-                  << " is empty. Please add at least one line.\n";
+            << " is empty. Please add at least one line.\n";
         return 1;
     }
 
@@ -97,18 +86,11 @@ int wmain() {
     std::getline(std::cin, token);
     if (token.empty()) return 1;
 
-    HINTERNET session = WinHttpOpen(
-        L"SelfBot/1.0",
-        WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
-        WINHTTP_NO_PROXY_NAME,
-        WINHTTP_NO_PROXY_BYPASS, 0
-    );
+    HINTERNET session = WinHttpOpen(L"SelfBot/1.0", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
     if (!session) return 1;
 
-    std::wstring authHdr = L"Authorization: " +
-        std::wstring(token.begin(), token.end()) + L"\r\n";
-    if (HttpRequest(session, L"discord.com", L"/api/v10/users/@me",
-        L"GET", authHdr, "") != 200) {
+    std::wstring authHdr = L"Authorization: " + std::wstring(token.begin(), token.end()) + L"\r\n";
+    if (HttpRequest(session, L"discord.com", L"/api/v10/users/@me", L"GET", authHdr, "") != 200) {
         std::cerr << "Invalid token, exiting.\n";
         return 1;
     }
@@ -136,9 +118,15 @@ int wmain() {
     char delMode = 'n';
     std::cin >> delMode;
     bool delete_after_send = (delMode == 'y' || delMode == 'Y');
+
+    int deleteSeconds = 0;
+    if (delete_after_send) {
+        std::cout << "Delete after how many seconds? ";
+        std::cin >> deleteSeconds;
+    }
     std::cout << "\n";
 
-    std::thread([&, delete_after_send, wChannelId]() {
+    std::thread([&, delete_after_send, deleteSeconds, wChannelId]() {
         std::mt19937_64 rng(std::random_device{}());
         std::uniform_int_distribution<int> dist(minI, maxI);
         size_t index = 0;
@@ -164,54 +152,42 @@ int wmain() {
                 body += (c == '"') ? "\\\"" : std::string(1, c);
             body += "\"}";
 
-            HINTERNET connect = WinHttpConnect(session, L"discord.com",
-                INTERNET_DEFAULT_HTTPS_PORT, 0);
-            HINTERNET request = WinHttpOpenRequest(
-                connect, L"POST",
-                (L"/api/v10/channels/" + wChannelId + L"/messages").c_str(),
-                nullptr, WINHTTP_NO_REFERER,
-                WINHTTP_DEFAULT_ACCEPT_TYPES,
-                WINHTTP_FLAG_SECURE
-            );
-            WinHttpSendRequest(request,
-                (authHdr + L"Content-Type: application/json\r\n").c_str(), (ULONG)-1,
-                (LPVOID)body.data(), (ULONG)body.size(),
-                (ULONG)body.size(), 0);
+            HINTERNET connect = WinHttpConnect(session, L"discord.com", INTERNET_DEFAULT_HTTPS_PORT, 0);
+            HINTERNET request = WinHttpOpenRequest(connect, L"POST", (L"/api/v10/channels/" + wChannelId + L"/messages").c_str(), nullptr, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE);
+            WinHttpSendRequest(request, (authHdr + L"Content-Type: application/json\r\n").c_str(), (ULONG)-1, (LPVOID)body.data(), (ULONG)body.size(), (ULONG)body.size(), 0);
             WinHttpReceiveResponse(request, nullptr);
 
             DWORD status = 0, len = sizeof(status);
-            WinHttpQueryHeaders(request,
-                WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
-                nullptr, &status, &len, nullptr);
+            WinHttpQueryHeaders(request, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER, nullptr, &status, &len, nullptr);
 
-            if (status == 200 || status == 201) {
-                log("Sent: \"" + msg + '"');
-
-                if (delete_after_send) {
-                    std::string response;
-                    DWORD avail = 0;
-                    while (WinHttpQueryDataAvailable(request, &avail) && avail > 0) {
-                        std::vector<char> buffer(avail);
-                        DWORD read = 0;
-                        WinHttpReadData(request, buffer.data(), avail, &read);
-                        response.append(buffer.data(), read);
-                    }
-                    std::string id;
-                    auto pos = response.find("\"id\":\"");
-                    if (pos != std::string::npos) {
-                        pos += 6;
-                        auto end = response.find('"', pos);
-                        if (end != std::string::npos)
-                            id = response.substr(pos, end - pos);
-                    }
-                    if (!id.empty()) {
-                        std::wstring delPath = L"/api/v10/channels/" + wChannelId + L"/messages/" +
-                                               std::wstring(id.begin(), id.end());
-                        HttpRequest(session, L"discord.com", delPath, L"DELETE",
-                                    authHdr, std::string());
-                        log("Deleted: message ID " + id);
-                    }
+            std::string response;
+            if ((status == 200 || status == 201) && delete_after_send) {
+                while (WinHttpQueryDataAvailable(request, &len) && len > 0) {
+                    std::vector<char> buffer(len);
+                    DWORD read = 0;
+                    WinHttpReadData(request, buffer.data(), len, &read);
+                    response.append(buffer.data(), read);
                 }
+                std::string id;
+                auto pos = response.find("\"id\":\"");
+                if (pos != std::string::npos) {
+                    pos += 6;
+                    auto end = response.find('"', pos);
+                    if (end != std::string::npos)
+                        id = response.substr(pos, end - pos);
+                }
+                if (!id.empty()) {
+                    std::thread([session, authHdr, wChannelId, id, deleteSeconds]() {
+                        std::this_thread::sleep_for(std::chrono::seconds(deleteSeconds));
+                        std::wstring path = L"/api/v10/channels/" + wChannelId + L"/messages/" + std::wstring(id.begin(), id.end());
+                        HttpRequest(session, L"discord.com", path, L"DELETE", authHdr, "");
+                        log("Deleted: message ID " + id);
+                        }).detach();
+                }
+                log("Sent: \"" + msg + "\"");
+            }
+            else if (status == 200 || status == 201) {
+                log("Sent: \"" + msg + "\"");
             }
             else {
                 log("Error sending (HTTP " + std::to_string(status) + ")");
@@ -229,7 +205,7 @@ int wmain() {
             std::lock_guard<std::mutex> lock(cout_mtx);
             std::cout << "\r                         \r";
         }
-    }).detach();
+        }).detach();
 
     std::this_thread::sleep_for(std::chrono::hours(24 * 365));
     return 0;
